07f62bcd0edad6f042f4149835fac5ffb4a89c4f
chandresh-patelsf <118016778+chandresh-patelsf@users.noreply.github.com>
2023-01-11 19:09:44 -0800

    Update start-bridgeclient.sh

--- start-bridgeclient.sh
+++ start-bridgeclient.sh
@@ -4,8 +4,6 @@ set -e -x
 export CLIENT=$HOSTNAME
 export PATTOKENID=$HOSTNAME
 export PATTOKENFILE="/etc/secret/pat"
-export PAT_TOKEN=$(cat "/etc/secret/pat" | python -c \
-    'import json,sys,os;print(json.load(sys.stdin)[os.getenv("HOSTNAME")])')
 
 /opt/tableau/tableau_bridge/bin/TabBridgeClientWorker -e \
     --client="${CLIENT}" \
